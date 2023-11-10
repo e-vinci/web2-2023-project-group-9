e6eabfc7ba0ca363f0e87c96b51592c41860c8b1
karim-naviaux <karim.naviaux@student.vinci.be>
2023-11-10 13:02:10 +0100

    add:LoginPage.js + code html

---
+++ frontend/src/Components/Pages/LoginPage.js
@@ -0,0 +1,32 @@
+// eslint-disable-next-line no-unused-vars
+import { clearPage, renderPageTitle } from '../../utils/render';
+
+const LoginPage = () => {
+  clearPage();
+
+  const main = document.querySelector('main');
+  main.innerHTML = renderLoginPage();
+
+  function renderLoginPage() {
+    const formLoginPage = `
+    <div class="container">
+        <div id="containerForm">
+            <form action="#" method="post">
+                <label for="pseudo">Pseudo / Email :</label>
+                <input type="text" id="pseudo" name="pseudo" required>
+                <br>
+                <label for="motdepasse">Mot de passe :</label>
+                <input type="password" id="motdepasse" name="motdepasse" required>
+                <br>
+                <p id="paragrapheLogin">Si vous n'avez pas de compte ? <a href="#"> Inscrivez-vous</a></p>
+                <br>
+                <input type="submit" value="Se connecter">
+            </form> 
+        </div> 
+    </div>`;
+
+    return formLoginPage;
+  };
+};
+
+export default LoginPage;
