baf44a82992c5074315be4c0d07723636653d094
mohamedguerbaoui <mohamed.guerbaoui@student.vinci.be>
2023-11-26 19:45:15 +0100

    feat: position fighters of FightersPage

--- frontend/src/Components/Pages/FightersPage.js
+++ frontend/src/Components/Pages/FightersPage.js
@@ -1,4 +1,4 @@
-// import anime from 'animejs';
+ import anime from 'animejs';
 
 import arena from '../../img/Arena/areneDBZ.png';
 import brolyPortrait from '../../img/PortraitFighters/Broly.png';
@@ -21,31 +21,101 @@ const FightersPage = () => {
 
   main.innerHTML += 
     `<section class="main-container">
-    <img src="${brolyPortrait}" alt="" >
-    <img src="${venomPortrait}" alt="" >
-    <img src="${spiderManPortrait}" alt="" >
-    <img src="${chunLiPortrait}" alt="" >
-    <img src="${captainAmerica}" alt="" >
-    <img src="${gokuPortrait}" alt="" >
-    <img src="${gohanPortrait}" alt="" >
+
+
+    <button style="background-image: url('${brolyPortrait}')" class="brolyDBZ"></button>
+    <button style="background-image: url('${venomPortrait}')" class="venomSP"></button>
+    <button style="background-image: url('${spiderManPortrait}')" class="spiderMan"></button>
+    <button style="background-image: url('${chunLiPortrait}')" class="chunLiTK"></button>
+    <button style="background-image: url('${captainAmerica}')" class="captainAmerica"></button>
+    <button style="background-image: url('${gokuPortrait}')" class="goku"></button>
+    <button style="background-image: url('${gohanPortrait}')" class="gohan"></button>
+    
   </section>
 
-
     
+    <section id="sprite">
+      <div id="brolyDBZ"></div>
+      <div id="venomSP"></div>
+      <div id="chunLiTK"></div>
+      <div id="captainAmerica"></div>
+      <div id="goku"></div>
+      <div id="gohan"></div>
+      <div id="spiderMan"></div>
     
-    
-    
-    <section id="spriteAcceuil">
-      <div id="broly"></div>
-      <div id="venom"></div>
-      <div id="chunLi"></div>
-      <div id="captainA"></div>
+    </section>
+    <section id="arena">
     <img src="${arena}" alt="" class="arena">
-    </section>`;
+    </section>   `;
+
+  main.appendChild(mainContainer);
+
+  const broly = document.querySelector('#brolyDBZ');
+  const venom = document.querySelector('#venomSP');
+  const chunLi = document.querySelector('#chunLiTK');
+  const captainA = document.querySelector('#captainAmerica');
+
+  createAnimation(broly, '-2006px 0px', 'steps(10)', 1500);
+  createAnimation(venom, '-3940px 0px', 'steps(17)', 1500);
+  createAnimation(chunLi, '-1302px 0px', 'steps(14)', 1500);
+  createAnimation(captainA, '-441px 0px', 'steps(3)', 1500);
+
+  const img = document.querySelectorAll('.main-container>button');
+  const animation = document.querySelectorAll('#sprite>div');
+
+  img.forEach((image) => {
+    image.addEventListener('click', () => {
+     animationImg(image);
+    });
+  });
+
+  function animationImg(nomImg) {
+    // eslint-disable-next-line no-shadow
+    animation.forEach((anime) => {
+      if (nomImg.className === anime.id) {
+        // eslint-disable-next-line no-param-reassign
+        anime.style.display = 'block';
+        // eslint-disable-next-line no-param-reassign
+        anime.style.backgroundPosition = '50px' ;
+      }
+    });
+    img.forEach((image) => {
+      image.addEventListener('click', () => {
+       animationImgHidden(image);
+      });
+    });
+  }
 
   
 
-  main.appendChild(mainContainer);
+  function animationImgHidden(nomImg) {
+    // eslint-disable-next-line no-shadow
+    animation.forEach((anime) => {
+      if (nomImg.className === anime.id) {
+        // eslint-disable-next-line no-param-reassign
+        anime.style.display = 'none';
+      }
+    });
+    img.forEach((image) => {
+      image.addEventListener('click', () => {
+       animationImg(image);
+      });
+    });
+  } 
+
+
+
+  function createAnimation(target, startPosition, easing, duration) {
+    return anime({
+      targets: target,
+      backgroundPosition: startPosition,
+      easing,
+      duration,
+      loop: true,
+    });
+  }
+
+
 
  /* const venom = document.querySelector('#venom');
   const chunLi = document.querySelector('#chunLi');
@@ -64,17 +134,9 @@ const FightersPage = () => {
     createAnimation(brolySurPlace, '-2006px 0px', 'steps(10)', 1500);
     // Affichage de l'arène
     arenaElement.style.display = 'block';
-  } 
-
-  function createAnimation(target, startPosition, easing, duration) {
-    return anime({
-      targets: target,
-      backgroundPosition: startPosition,
-      easing,
-      duration,
-      loop: true,
-    });
   } */
+
 }; 
+
 
 export default FightersPage;
