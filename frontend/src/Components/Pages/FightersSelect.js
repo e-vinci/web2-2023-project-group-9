55f80c472049260890de192a3a29e61273d4bf21
sohaib <sohaib.elbouhtani@student.vinci.be>
2023-11-29 16:06:42 +0100

    add: background on FighterSelectPage

--- frontend/src/Components/Pages/FightersSelect.js
+++ frontend/src/Components/Pages/FightersSelect.js
@@ -1,7 +1,16 @@
+import backgroundImage from '../../img/BaseDuSite/BackgroundFightersSelect.jpg'
 import { clearPage } from '../../utils/render';
 
 const FightersSelect  = () =>{
     clearPage();
+    const main = document.querySelector('main');
+    const body = document.body.style;
+    main.innerHTML = ' '
+    body.backgroundImage = `url(${backgroundImage})`;
+    body.maxWidth = '100%';
+    body.backgroundRepeat = 'no-repeat';
+    body.backgroundPosition = 'center top';
+    body.backgroundSize = 'cover';
 };
 
 export default FightersSelect;
