956c824a8740510459a010789ac42fe23e242113
karim-naviaux <karim.naviaux@student.vinci.be>
2023-11-30 10:29:27 +0100

    add:mecanic of game

---
+++ frontend/src/utils/phrasesGame.js
@@ -0,0 +1,30 @@
+    // eslint-disable-next-line import/prefer-default-export
+    export const getPhraseRandom = async () => {
+        try {
+            const response = await fetch('../data/phrases.json');
+
+            if (!response.ok) {
+                throw new Error(`fetch error : ${response.status} : ${response.statusText}`);
+            }
+
+            const result = await response.json();
+
+            console.log('JSON result:', result); // Ajoutez cette ligne pour déboguer
+
+            const phrasesArray = result.phrases;
+
+            if (!phrasesArray || phrasesArray.length === 0) {
+                throw new Error('Empty or missing "phrases" array in JSON.');
+            }
+
+            const phraseAleatoire = phrasesArray[Math.floor(Math.random() * phrasesArray.length)];
+
+            console.log('Random phrase:', phraseAleatoire); // Ajoutez cette ligne pour déboguer
+
+            return phraseAleatoire;
+        } catch (error) {
+            console.error('Error in getPhraseRandom:', error);
+            return 'Erreur : Impossible de récupérer la phrase.';
+        }
+    };
+
