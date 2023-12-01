f4c87d90c1fed3ce9c38b1b18da815c063494b58
sohaib <sohaib.elbouhtani@student.vinci.be>
2023-12-01 01:03:56 +0100

    clearPage of FighterSelect

--- frontend/src/Components/Pages/FightersSelect.js
+++ frontend/src/Components/Pages/FightersSelect.js
@@ -1,16 +1,7 @@
-import backgroundImage from '../../img/BaseDuSite/BackgroundFightersSelect.jpg'
-import { clearPage } from '../../utils/render';
+import { clearAllPage } from '../../utils/render';
 
 const FightersSelect  = () =>{
-    clearPage();
-    const main = document.querySelector('main');
-    const body = document.body.style;
-    main.innerHTML = ' '
-    body.backgroundImage = `url(${backgroundImage})`;
-    body.maxWidth = '100%';
-    body.backgroundRepeat = 'no-repeat';
-    body.backgroundPosition = 'center top';
-    body.backgroundSize = 'cover';
+    clearAllPage();
 };
 
 export default FightersSelect;
