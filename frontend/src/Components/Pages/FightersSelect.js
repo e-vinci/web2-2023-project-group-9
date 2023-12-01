57956228759d5f7d03c4b9c25ef785d5f1ffb6a5
sohaib2 <sohai@DESKTOP-VR38C7O.(none)>
2023-12-01 11:42:44 +0100

    add: background for FightersSelect

--- frontend/src/Components/Pages/FightersSelect.js
+++ frontend/src/Components/Pages/FightersSelect.js
@@ -1,7 +1,13 @@
 import { clearAllPage } from '../../utils/render';
 
+import background from '../../img/BaseDuSite/backgroundFightSelect.jpg';
+
+const body = document.querySelector('body');
+ 
 const FightersSelect  = () =>{
     clearAllPage();
+    body.style.backgroundImage = `url(${background})`;
+    body.style.backgroundSize = 'cover';
 };
 
 export default FightersSelect;
