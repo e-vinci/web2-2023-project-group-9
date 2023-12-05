1a6ff666a68442f4b0995f3cdce323d3f5aa4e34
sohaib <sohaib.elbouhtani@student.vinci.be>
2023-12-05 10:43:58 +0100

    add: finish DemoPage and modification css

--- frontend/src/Components/Pages/DemoGamePage.js
+++ frontend/src/Components/Pages/DemoGamePage.js
@@ -5,8 +5,8 @@
 /* eslint-disable import/order */
 /* eslint-disable import/no-extraneous-dependencies */
 
-// import { driver } from 'driver.js';
-// import 'driver.js/dist/driver.css';
+import { driver } from 'driver.js';
+import 'driver.js/dist/driver.css';
 import anime from 'animejs';
 // import Navigate from '../Router/Navigate';
 import { clearAllPage } from '../../utils/render';
@@ -124,6 +124,7 @@ const DemoGame = () => {
       setTimeout(() =>{
         charactereOfPlayer1.classList.add('transformVenom');
         charactereOfPlayer2.classList.add('transformBroly');
+        functionDriver();
       },200);
     }
   }, 500)};
@@ -156,47 +157,70 @@ const DemoGame = () => {
   //     });
   // }
 
-  // const d = driver({
-  //   showProgress: true,
-  //   popoverClass: "driverjs-theme",
-  //   steps: [
-  //     {
-  //       element: '#arene-demo',
-  //       popover: {
-  //         title: 'Arène',
-  //         description: "Choix de l'arène fait par les 2 joueurs : Arène de Dragon Ball Z",
-  //         align: 'start',
-  //       },
-  //     },
-  //     {
-  //       element: '#charactere-player-1-demo',
-  //       popover: {
-  //         title: 'Joueur 1',
-  //         description: 'Choix du personnage fait par le joueur 1 : Venom',
-  //         side: 'right',
-  //         align: 'center',
-  //       },
-  //     },
-  //     {
-  //       element: '#charactere-player-2-demo',
-  //       popover: {
-  //         title: 'Joueur 2',
-  //         description: 'Choix du personnage fait par le joueur 2 : Broly',
-  //         side: 'left',
-  //         align: 'center',
-  //       },
-  //     },
-  //     {
-  //       element: '#phrase-attribue-demo',
-  //       popover: {
-  //         title: 'Phrase à écrire pour le joueur 1',
-  //         description: 'Chaque joueur aura son tour pour écrire la phrase qui lui aura été attribué de manière au hazard'
-  //       },
-  //     },
-  //   ],
-  // });
+  function functionDriver(){
+    const d = driver({
+      showProgress: true,
+      popoverClass: "driverjs-theme",
+      steps: [
+        {
+          element: '.home',
+          popover: {
+            title: 'Menu Game',
+            description: 'Menu du jeux qui permettra aux joueurs de continuer, recommencer ou quitter la partie',
+            side: 'bottom'
+          }
+        },
+        {
+          element: '#arene-demo',
+          popover: {
+            title: 'Arène',
+            description: "Choix de l'arène fait par les 2 joueurs",
+            align: 'start',
+          },
+        },
+        {
+          element: '#charactere-player-1-demo',
+          popover: {
+            title: 'Joueur 1',
+            description: 'Choix du personnage fait par le joueur 1 : Venom',
+            side: 'right',
+            align: 'center',
+          },
+        },
+        {
+          element: '#charactere-player-2-demo',
+          popover: {
+            title: 'Joueur 2',
+            description: 'Choix du personnage fait par le joueur 2 : Broly',
+            side: 'left',
+            align: 'center',
+          },
+        },
+        {
+          element: '#phrase-attribue-demo',
+          popover: {
+            title: 'Phrase à écrire pour le joueur 1',
+            description: 'Chaque joueur aura son tour pour écrire la phrase qui lui aura été attribué de manière au hazard'
+          },
+        },
+        {
+          element : '.lifeBarContainerLeft',
+          popover: {
+            title: 'Barre de vie du joueur',
+            description: 'A chaque tour le joueur verra sa vie baissée, si'
+          }
+        },
+        {
+          element: '.timeLeft',
+          popover: {
+            title: 'Le temps pris pour écrire la phrase est inférieur au temps du joueur 2'
+          }
+        }
+      ],
+    });
 
-  // d.drive();
+    d.drive();
+  }
 };
 
 export default DemoGame;
