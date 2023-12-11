fbcb144331beed2e07350a15681ce76ad6b9fdce
sohaib <sohaib.elbouhtani@student.vinci.be>
2023-12-11 03:13:32 +0100

    fix: problem with restart bottom, add the sprite of Venom and Broly, Fix the problem with Height of sprite, add timer and function write, add de color winner and loser and add animation on spot of two fighters

--- frontend/src/Components/Pages/DemoGamePage.js
+++ frontend/src/Components/Pages/DemoGamePage.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 /* eslint-disable prefer-arrow-callback */
 /* eslint-disable no-plusplus */
 /* eslint-disable no-console */
@@ -14,7 +15,7 @@ import anime from 'animejs';
 import { clearAllPage } from '../../utils/render';
 import Navigate from '../Router/Navigate';
 
-import backgroundDemo from '../../img/Arena/arene1.gif';
+import backgroundDemo from '../../img/Arena/arene9.gif';
 import vsFight from '../../img/Arena/vsFight.png';
 // import {driver} from 'driver.js';
 // import 'driver.js/dist/driver.css';
@@ -54,35 +55,109 @@ const DemoGame = () => {
         <div>
         <img src='${vsFight}' class='vs'></img>
         </div>
-        <div class='timeRight'>00:00</div>
+        <div class='timeRight'>00:15</div>
         <div class="lifeBarContainerRight">
             <div class="lifeBarRightRed"></div>
             <div class="lifeBarRightGreen"></div>
         </div>
       </section>
-      <div id="charactere-player-1-demo"></div>
-      <div id="charactere-player-2-demo"></div>
+      <div id="charactere-player-1-demo" class="baseV"></div>
+      <div id="charactere-player-2-demo" class="baseB"></div>
   </div>
-  <div id="phrase-demo"><p id="phrase-attribue-demo">Le monde regarde, sois un champion!</p></div>`;
-  loadingPage();
+  <div id="phrase-demo"><p id="phrase-attribue-demo">La guerre ne détermine pas qui a raison, mais qui reste.</p></div>`;
   const charactereOfPlayer2 = document.querySelector('#charactere-player-2-demo');
   const charactereOfPlayer1 = document.querySelector('#charactere-player-1-demo');
-
+  const phrase = document.querySelector('#phrase-demo');
   const menuOpen = document.querySelector('#menu');
+  const times = document.querySelectorAll('.timeLeft, .timeRight');
+  const timeLeft = document.querySelector('.timeLeft');
+  const text = document.querySelector('#phrase-attribue-demo');
 
-  let times = document.querySelectorAll('.timeLeft, .timeRight');
+  let timer;
+  let countdown;
+  let contentOfText = text.textContent;
+  let letters = contentOfText.split('');
+  let spans;
+
+  loadingPage();
+
+  function startCountdown() {
+    let count = 3;
+    countdown = setInterval(() => {
+      if (count > 0) {
+        timeLeft.textContent = count.toString();
+        count--;
+      } else {
+        clearInterval(countdown);
+        startTimer();
+      }
+    }, 1000);
+  }
+
+  function startTimer() {
+    write(0);
+    let time = 0;
+    timer = setInterval(() => {
+      time++;
+      let minutes = Math.floor(time / 60);
+      let seconds = time % 60;
+      timeLeft.textContent = `${minutes.toString().padStart(2, '0')}:${seconds
+        .toString()
+        .padStart(2, '0')}`;
+    }, 1000);
+  }
+
+  function write(currentIndex, resetIndex = false) {
+    text.innerHTML = letters.map((letter) => `<span>${letter}</span>`).join('');
+    spans = text.querySelectorAll('span');
+    if(resetIndex){
+      document.removeEventListener('keydown',keydownHandler);
+    }
+    function keydownHandler(e) {
+      if (currentIndex < contentOfText.length) {
+        if (e.key.length === 1) {
+          let letterSpan = spans[currentIndex];
+          if (e.key === contentOfText[currentIndex]) {
+            letterSpan.style.color = 'green';
+            if (!resetIndex) {
+              currentIndex++;
+            }else{
+              currentIndex = 1
+              resetIndex = false;
+            }            
+            if (currentIndex === contentOfText.length) {
+              clearInterval(timer);
+              let timerFinal = [];
+              times.forEach((time) => {
+                timerFinal.push(time.innerHTML);
+              });
+              let result = compareTime(timerFinal);
+              setColors(result);
+            }
+          } else {
+            letterSpan.style.color = 'red';
+          }
+        }
+      }
+    }
+    document.addEventListener('keydown', keydownHandler);
+    return keydownHandler;
+  }
 
   document.querySelector('#restartLink').addEventListener('click', (e) => {
     e.preventDefault();
     menuOpen.style.top = '-100%';
-    setTimeout(() => {
-      charactereOfPlayer1.classList.remove('transformVenom');
-      charactereOfPlayer2.classList.remove('transformBroly');
-      times.forEach((t) => {
-        t.innerHTML = '00:00';
-      });
-      loadingPage();
-    }, 500);
+    phrase.style.top = '50%';
+    charactereOfPlayer1.classList.remove('transformVenom');
+    charactereOfPlayer2.classList.remove('transformBroly');
+    clearInterval(timer); 
+    clearInterval(countdown);
+    timeLeft.innerHTML = '00:00';
+    times.forEach((time) => {
+      time.style.color = 'white';
+    });
+    write(0, true);
+    loadingPage();
   });
 
   document.querySelector('#accueilLink').addEventListener('click', (e) => {
@@ -124,44 +199,29 @@ const DemoGame = () => {
         loading.style.display = 'none';
         valueLoading.innerHTML = '3';
         setTimeout(() => {
-          charactereOfPlayer1.classList.add('transformVenom');
-          charactereOfPlayer2.classList.add('transformBroly');
           functionDriver();
         }, 200);
       }
     }, 500);
   }
 
-  transformation(charactereOfPlayer2, 1500);
-  transformation(charactereOfPlayer1, 1500);
-
   function transformation(target, duration) {
     return anime({
       targets: target,
-      loop: true,
       duration,
+      complete: () => {
+        console.log('Première animation terminée.');
+        charactereOfPlayer1.classList.add('onTheSpotVenom1');
+        charactereOfPlayer1.classList.add('onTheSpotVenom');
+        charactereOfPlayer2.classList.add('onTheSpotBroly');
+      },
     });
   }
 
-  // charactereOfPlayer2.classList.remove('transformBroly');
-  // charactereOfPlayer2.classList.add('moveBroly');
-
-  // shift(charactereOfPlayer2,1500);
-
-  // function shift(target,duration) {
-  //     return anime({
-  //         targets: target,
-  //         translateX: {
-  //           value: '-=600px', // changez cette valeur pour contrôler la distance de déplacement
-  //           duration,
-  //         },
-  //         easing : 'linear',
-  //         loop: false,
-  //     });
-  // }
-
   function functionDriver() {
+    let countdownStarted = false;
     const d = driver({
+      showButtons: ['next', 'previous', 'close'],
       showProgress: true,
       popoverClass: 'driverjs-theme',
       steps: [
@@ -212,87 +272,108 @@ const DemoGame = () => {
           element: '.lifeBarContainerLeft',
           popover: {
             title: 'Barre de vie du joueur',
-            description: 'A chaque tour le joueur verra sa vie baissée, si',
+            description:
+              'A chaque tour le joueur verra sa vie baissée, si le temps pris pour écrire la phrase est inférieur au temps du joueur 2',
           },
         },
         {
           element: '.timeLeft',
           popover: {
             title: 'Le temps',
-            description: 'pris pour écrire la phrase est inférieur au temps du joueur 2'
+            description:
+              "il y aura un temps de préparation de 5 secondes pour chaque joueur suivi par un cronomètre qui s'arrétera des que le joueur entre la dernière lettre de la phrase",
           },
         },
         {
-            element: '#phrase-attribue-demo',
-            popover: {
-              title: 'Phrase à écrire',
-              description: 'A vous de jouer, vous incarnez Venom !',
-            },
+          element: '.timeRight',
+          popover: {
+            title: 'Le temps',
+            description: 'Nous supposons que le joueur 2 à taper une phrase avec à un temps de',
+          },
+        },
+        {
+          element: '#phrase-attribue-demo',
+          popover: {
+            title: 'La phrase à taper',
+            description:
+              'A vous de jouer, vous incarnez Venom, le compte à rebours commencera à la fin du Tutoriel, Enjoy !',
+          },
         },
       ],
+      onDestroyed: () => {
+        if (!countdownStarted) { // Démarrer le compte à rebours uniquement si ce n'est pas déjà en cours
+          startCountdown();
+          countdownStarted = true;
+        }
+        transformation(charactereOfPlayer2, 1500);
+        transformation(charactereOfPlayer1, 1500);
+        charactereOfPlayer1.classList.add('transformVenom');
+        charactereOfPlayer2.classList.add('transformBroly');
+      },
+      onCloseClick: () => {
+        d.destroy();
+        if (!countdownStarted) { // Démarrer le compte à rebours uniquement si ce n'est pas déjà en cours
+          startCountdown();
+          countdownStarted = true;
+        }
+        transformation(charactereOfPlayer2, 1500);
+        transformation(charactereOfPlayer1, 1500);
+        charactereOfPlayer1.classList.add('transformVenom');
+        charactereOfPlayer2.classList.add('transformBroly');
+      },
     });
     d.drive();
   }
 
-  let text = document.querySelector('#phrase-attribue-demo');
-  text.innerHTML = "Je m'appelle Sohaib";
-  let contentOfText = text.textContent;
-  let letters = contentOfText.split('');
+  function compareTime(timerFinal) {
+    const timeInSeconds = timerFinal.map((timeString) => {
+      const [minutes, seconds] = timeString.split(':').map(Number);
+      return minutes * 60 + seconds;
+    });
 
-  text.innerHTML = letters.map((letter) => `<span>${letter}</span>`).join('');
-
-  let currentIndex = 0;
-  let timer;
-  let countdown;
-  let countdownStarted = false;
-
-  const spans = text.querySelectorAll('span');
-  const timeLeft = document.querySelector('.timeLeft');
-
-  function startTimer() {
-    let time = 0;
-    timer = setInterval(() => {
-      time++;
-      let minutes = Math.floor(time / 60);
-      let seconds = time % 60;
-      timeLeft.textContent = `${minutes.toString().padStart(2, '0')}:${seconds
-        .toString()
-        .padStart(2, '0')}`;
-    }, 1000);
-  }
-
-  function startCountdown() {
-    let count = 3;
-    countdown = setInterval(() => {
-      if (count > 0) {
-        timeLeft.textContent = count.toString();
-        count--;
-      } else {
-        clearInterval(countdown);
-        startTimer();
-      }
-    }, 1000);
-  }
-
-  document.addEventListener('keydown', (e) => {
-    if (currentIndex < contentOfText.length) {
-      if (e.key.length === 1) {
-        if(!countdown){
-          startCountdown();
-        }
-        let letterSpan = spans[currentIndex];
-        if (e.key === contentOfText[currentIndex]) {
-          letterSpan.style.color = 'green';
-          currentIndex++;
-          if (currentIndex === contentOfText.length) {
-            clearInterval(timer);
-          }
-        } else {
-          letterSpan.style.color = 'red';
-        }
-      }
+    // Comparer les temps
+    if (timeInSeconds[0] < timeInSeconds[1]) {
+      return 'player1'; // Joueur 1 a mis moins de temps
     }
-  });
+    if (timeInSeconds[0] > timeInSeconds[1]) {
+      return 'player2'; // Joueur 2 a mis moins de temps
+    }
+    return 'equal'; // Les joueurs ont mis le même temps
+  }
+
+  function setColors(result) {
+    if (result === 'player1') {
+      times[0].style.color = 'green';
+      times[1].style.color = 'red';
+      phrase.style.top = '-100%';
+    } else if (result === 'player2') {
+      times[0].style.color = 'red';
+      times[1].style.color = 'green';
+      phrase.style.top = '-100%';
+    } else {
+      phrase.style.top = '-100%';
+      times.forEach((time) => {
+        time.style.color = 'orange';
+      });
+    }
+  }
+
+  // charactereOfPlayer2.classList.remove('transformBroly');
+  // charactereOfPlayer2.classList.add('moveBroly');
+
+  // shift(charactereOfPlayer2,1500);
+
+  // function shift(target,duration) {
+  //     return anime({
+  //         targets: target,
+  //         translateX: {
+  //           value: '-=600px', // changez cette valeur pour contrôler la distance de déplacement
+  //           duration,
+  //         },
+  //         easing : 'linear',
+  //         loop: false,
+  //     });
+  // }
 };
 
 export default DemoGame;
